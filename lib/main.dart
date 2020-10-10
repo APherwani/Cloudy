a6f7d255db17b7dd5b046ccae162937e2984d6a4
Elbert <etse168@gmail.com>
2020-10-10 17:49:29 -0400

    Working on display

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+// Written by Elbert Tse and Arjun Pherwani for KnightHacks 2020
+
+// ADD LOCATION PERMISSION STUFF TO ANDROID AND IOS FOLDERS
+
 import 'package:cloudy/splash.dart';
 import 'package:flutter/material.dart';
 
@@ -9,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: true,
       theme: ThemeData.dark(),
       home: SplashPage(),
     );
