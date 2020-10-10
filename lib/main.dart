5b3746b217e9b1262aeee46c3ac44d1bc8b938bb
arjunpherwani <arjunp@knights.ucf.edu>
2020-10-10 18:22:50 -0400

    Got location and weather data working

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,34 @@
 import 'package:cloudy/splash.dart';
 import 'package:flutter/material.dart';
+import 'package:cloudy/weather.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  void printWeather() async {
+    Weather weather = Weather();
+    var str = await weather.getWeather();
+    print(str);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark(),
-      home: SplashPage(),
+      home: Scaffold(
+        body: Center(
+          child: RaisedButton(
+            onPressed: () {
+              printWeather();
+            },
+            child: Text(
+              'print weather info',
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
